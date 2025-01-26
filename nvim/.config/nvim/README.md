a119601c78098f5d2de983b2c2f17fcc9ada85b3
pilladipesh33 <pilladipesh.pd@gmail.com>
2025-01-26 23:39:38 +0530

    markdown config updated

--- nvim/.config/nvim/README.md
+++ nvim/.config/nvim/README.md
@@ -4,3 +4,28 @@
 
 A starter template for [LazyVim](https://github.com/LazyVim/LazyVim).
 Refer to the [documentation](https://lazyvim.github.io/installation) to get started.
+
+```go
+#some go code
+fmt.Println("hi")
+```
+
+| 1   | 2   |
+| --- | --- |
+| one | two |
+
+> "something important"
+
+##### Todo list
+
+-[ ] create a readme
+
+- [x] added render markdown plugin
+
+> [!NOTE]
+> important
+
+> [!TIP]
+> tips of the day
+
+> [!WARNING] > [!DANGER] > [!SUCCESS]
--- nvim/.config/nvim/lua/plugins/markdown.lua
+++ nvim/.config/nvim/lua/plugins/markdown.lua
@@ -1,65 +1,62 @@
 return {
-  "MeanderingProgrammer/render-markdown.nvim",
-  enabled = true,
-  -- dependencies = { "nvim-treesitter/nvim-treesitter", "echasnovski/mini.nvim" }, -- if you use the mini.nvim suite
-  dependencies = { "nvim-treesitter/nvim-treesitter", "echasnovski/mini.icons" }, -- if you use standalone mini plugins
-  ---@module 'render-markdown'
-  ---@type render.md.UserConfig
-  opts = {
-    bullet = {
-      -- Turn on / off list bullet rendering
-      enabled = true,
+  {
+    "MeanderingProgrammer/render-markdown.nvim",
+    enabled = true,
+    -- dependencies = { "nvim-treesitter/nvim-treesitter", "echasnovski/mini.nvim" }, -- if you use the mini.nvim suite
+    dependencies = { "nvim-treesitter/nvim-treesitter", "echasnovski/mini.icons" }, -- if you use standalone mini plugins
+    ---@module 'render-markdown'
+    ---@type render.md.UserConfig
+    opts = {
+      bullet = {
+        -- Turn on / off list bullet rendering
+        enabled = true,
+      },
+      checkbox = {
+        -- Turn on / off checkbox state rendering
+        enabled = true,
+        -- Determines how icons fill the available space:
+        --  inline:  underlying text is concealed resulting in a left aligned icon
+        --  overlay: result is left padded with spaces to hide any additional text
+        position = "inline",
+        unchecked = {
+          -- Replaces '[ ]' of 'task_list_marker_unchecked'
+          icon = "   󰄱 ",
+          -- Highlight for the unchecked icon
+          highlight = "RenderMarkdownUnchecked",
+          -- Highlight for item associated with unchecked checkbox
+          scope_highlight = nil,
+        },
+        checked = {
+          -- Replaces '[x]' of 'task_list_marker_checked'
+          icon = "   󰱒 ",
+          -- Highlight for the checked icon
+          highlight = "RenderMarkdownChecked",
+          -- Highlight for item associated with checked checkbox
+          scope_highlight = nil,
+        },
+      },
+      html = {
+        -- Turn on / off all HTML rendering
+        enabled = true,
+        comment = {
+          -- Turn on / off HTML comment concealing
+          conceal = false,
+        },
+      },
+      heading = {
+        sign = false,
+        icons = { "󰎤 ", "󰎧 ", "󰎪 ", "󰎭 ", "󰎱 ", "󰎳 " },
+      },
     },
-    checkbox = {
-      -- Turn on / off checkbox state rendering
-      enabled = true,
-      -- Determines how icons fill the available space:
-      --  inline:  underlying text is concealed resulting in a left aligned icon
-      --  overlay: result is left padded with spaces to hide any additional text
-      position = "inline",
-      unchecked = {
-        -- Replaces '[ ]' of 'task_list_marker_unchecked'
-        icon = "   󰄱 ",
-        -- Highlight for the unchecked icon
-        highlight = "RenderMarkdownUnchecked",
-        -- Highlight for item associated with unchecked checkbox
-        scope_highlight = nil,
-      },
-      checked = {
-        -- Replaces '[x]' of 'task_list_marker_checked'
-        icon = "   󰱒 ",
-        -- Highlight for the checked icon
-        highlight = "RenderMarkdownChecked",
-        -- Highlight for item associated with checked checkbox
-        scope_highlight = nil,
-      },
-    },
-    html = {
-      -- Turn on / off all HTML rendering
-      enabled = true,
-      comment = {
-        -- Turn on / off HTML comment concealing
-        conceal = false,
-      },
-    },
-    heading = {
-      sign = false,
-      icons = { "󰎤 ", "󰎧 ", "󰎪 ", "󰎭 ", "󰎱 ", "󰎳 " },
-      backgrounds = {
-        "Headline1Bg",
-        "Headline2Bg",
-        "Headline3Bg",
-        "Headline4Bg",
-        "Headline5Bg",
-        "Headline6Bg",
-      },
-      foregrounds = {
-        "Headline1Fg",
-        "Headline2Fg",
-        "Headline3Fg",
-        "Headline4Fg",
-        "Headline5Fg",
-        "Headline6Fg",
+  },
+  {
+    "iamcco/markdown-preview.nvim",
+    keys = {
+      {
+        "<leader>mp",
+        ft = "markdown",
+        "<cmd>MarkdownPreviewToggle<cr>",
+        desc = "Markdown Preview",
       },
     },
   },
